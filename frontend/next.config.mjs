c246f1d7b570ff4219ca8e7f753151f277e9007b
prathamdupare <prathamdupare@proton.me>
2024-06-25 03:04:08 +0530

    added nextra blog

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,21 @@
-// @ts-check
+/** @type {import('next').NextConfig} */
 
-/**
- * @type {import('next').NextConfig}
- */
+import nextra from 'nextra';
+
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ['ap-south-1.graphassets.com'],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
-
-export default nextConfig;
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+});
+export default withNextra(nextConfig);
