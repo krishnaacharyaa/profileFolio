5e0c5cd75c3a5f8312f92c2ba086a56f3f8d8c99
krishnaacharyaa <krishnaacharya.official@gmail.com>
2024-06-25 22:16:34 +0530

    chore: Enhanced frontend container image

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,14 +7,13 @@ const nextConfig = {
     domains: ['ap-south-1.graphassets.com'],
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
   },
+  output: 'standalone'
 };
+
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
 });
 export default withNextra(nextConfig);
+
