0048a954a346217f72963572b33ef97124bdca65
prathamdupare <prathamdupare@proton.me>
2024-06-25 13:17:32 +0530

    Remove 'ignore build errors' flag from config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -11,7 +11,6 @@ const nextConfig = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
   },
 };
 const withNextra = nextra({
