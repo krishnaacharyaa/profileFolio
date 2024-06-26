0e5a53bec686d577c3430bc624408e8a22f3e0e0
krishnaacharyaa <krishnaacharya.official@gmail.com>
2024-06-26 16:05:05 +0530

    fix: Fixed build errors

--- frontend/app/actions/user-actions.ts
+++ frontend/app/actions/user-actions.ts
@@ -1,18 +1,21 @@
 'use server';
 
 import { fetchWithLogging } from '../helper/logger';
+import { SkillRef } from '../types/skillRef';
+import { User } from '../types/user';
 
 export async function getUserData() {
-  // This no-store should be remove in future and replaced with revalidateTag,
-  // Adding now because it is wieredly caching the response, changes in the 8080/api/user is not reflected here if no-store isn't added
-  const response = await fetchWithLogging('http://localhost:8080/api/user', { cache: 'no-store' });
+  const response = await fetchWithLogging<User>('http://localhost:8080/api/user', {
+    // This no-store should be removed in future and replaced with revalidateTag,
+    // Adding now because it is wieredly caching the response, changes in the 8080/api/user is not reflected here if no-store isn't added
+    cache: 'no-store',
+  });
   return response.response;
 }
 
 export async function getSkillsData() {
-  // This no-store should be remove in future and replaced with revalidateTag,
-  // Adding now because it is wieredly caching the response, changes in the 8080/api/user is not reflected here if no-store isn't added
-  const response = await fetchWithLogging('http://localhost:8080/api/skills', {
+  const response = await fetchWithLogging<SkillRef[]>('http://localhost:8080/api/skills', {
+    // This no-store should be removed in future and replaced with revalidateTag,
     cache: 'no-store',
   });
   return response.response;
