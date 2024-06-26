903b5c5e9a015d74460904f3a7f511f8c80382a3
krishnaacharyaa <krishnaacharya.official@gmail.com>
2024-06-26 14:05:17 +0530

    feat: Change in schema to reflect in frontend

--- frontend/app/actions/user-actions.ts
+++ frontend/app/actions/user-actions.ts
@@ -1,11 +1,15 @@
 'use server';
 export async function getUserData() {
-  const res = await fetch('http://localhost:8080/api/user');
+  // This no-store should be remove in future and replaced with revalidateTag,
+  // Adding now because it is wieredly caching the response, changes in the 8080/api/user is not reflected here if no-store isn't added
+  const res = await fetch('http://localhost:8080/api/user', { cache: 'no-store' });
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
+    console.log({ res });
     throw new Error('Failed to fetch data');
   }
-
-  return res.json();
+  const result = await res.json();
+  console.log(result);
+  return result;
 }
--- frontend/app/components/profile-card.tsx
+++ frontend/app/components/profile-card.tsx
@@ -1,55 +1,73 @@
 import { User } from '../types/user';
-export default function ProfileCard({ user }: { user: User }) {
+
+interface ProfileCardProps {
+  user: User;
+}
+
+export default function ProfileCard({ user }: ProfileCardProps) {
   return (
     <div className="p-5">
       <h1 className="text-4xl font-bold mb-5">{user.basics.name}</h1>
       <p className="mb-5">{user.basics.summary}</p>
+
       <div className="mb-5">
         <h2 className="text-2xl font-semibold">Profiles</h2>
         {user.basics.profiles?.map((profile, index) => (
           <p key={index}>
-            <a href={profile.url} className="text-blue-500">
+            <a
+              href={profile.url}
+              className="text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {profile.network}: {profile.username}
             </a>
           </p>
         ))}
       </div>
+
       <div className="mb-5">
         <h2 className="text-2xl font-semibold">Work</h2>
         {user.work?.map((job, index) => (
-          <div key={index}>
+          <div key={index} className="mb-5">
             <p className="font-bold">
               {job.position} at{' '}
-              <a href={job.url} className="text-blue-500">
+              <a href={job.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">
                 {job.name}
               </a>
             </p>
             <p>
-              {job.startDate} - {job.endDate}
+              {job.startDate.toString()} - {job.endDate?.toString() || 'Present'}
             </p>
             <p>{job.summary}</p>
             <ul className="list-disc ml-5">
-              {job.highlights.map((highlight, i) => (
+              {job.highlights?.map((highlight, i) => (
                 <li key={i}>{highlight}</li>
               ))}
             </ul>
           </div>
         ))}
       </div>
+
       <div className="mb-5">
         <h2 className="text-2xl font-semibold">Education</h2>
         {user.education?.map((edu, index) => (
-          <div key={index}>
+          <div key={index} className="mb-5">
             <p className="font-bold">
               {edu.studyType} in {edu.area} at{' '}
-              <a href={edu.url} className="text-blue-500">
+              <a
+                href={edu.url || ''}
+                className="text-blue-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {edu.institution}
               </a>
             </p>
             <p>
-              {edu.startDate} - {edu.endDate}
+              {edu.startDate} - {edu.endDate || 'Present'}
             </p>
-            <p>Score: {edu.score}</p>
+            <p>Score: {edu.score || '-'}</p>
             <ul className="list-disc ml-5">
               {edu.courses?.map((course, i) => (
                 <li key={i}>{course}</li>
@@ -58,17 +76,25 @@ export default function ProfileCard({ user }: { user: User }) {
           </div>
         ))}
       </div>
+
       <div>
         <h2 className="text-2xl font-semibold">Projects</h2>
         {user.projects?.map((project, index) => (
-          <div key={index}>
+          <div key={index} className="mb-5">
+            hello
+            <div>{project.deployedUrl}</div>
             <p className="font-bold">
-              <a href={project?.githubUrl} className="text-blue-500">
+              <a
+                href={project.githubUrl || ''}
+                className="text-blue-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {project.name}
               </a>
             </p>
             <p>
-              {project.startDate} - {project.endDate}
+              {project.startDate.toString()} - {project.endDate?.toString() || 'Present'}
             </p>
             <p>{project.description}</p>
             <ul className="list-disc ml-5">
