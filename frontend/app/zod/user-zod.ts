3054c4b9baacf6c6f32c70b82df3237f4abad323
krishnaacharyaa <krishnaacharya.official@gmail.com>
2024-06-26 13:07:12 +0530

    feat: Made changes for the zod schema in frontend

--- frontend/app/zod/user-zod.ts
+++ frontend/app/zod/user-zod.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import { z } from 'zod';
 
 const LocationSchema = z.object({
@@ -19,78 +20,83 @@ const ProfileSchema = z.object({
 const WorkSchema = z.object({
   name: z.string({ required_error: 'Company name is required' }),
   position: z.string({ required_error: 'Position is required' }),
-  url: z.string({ required_error: 'URL is required' }).url('Invalid URL format'),
-  startDate: z.string({ required_error: 'Start date is required' }),
-  endDate: z.string().optional(),
+  url: z.string().url('Invalid URL format'),
+  startDate: z.date({ required_error: 'Start date is required' }),
+  endDate: z.date().nullable(),
   summary: z.string({ required_error: 'Summary is required' }),
-  highlights: z
-    .array(z.string({ required_error: 'Highlight cannot be empty' }))
-    .nonempty('At least one highlight is required'),
+  highlights: z.array(z.string()).nullable(),
 });
 
 const EducationSchema = z.object({
   institution: z.string({ required_error: 'Institution name is required' }),
-  url: z.string().url('Invalid URL format').optional(),
+  url: z.string().url('Invalid URL format').nullable(),
   area: z.string({ required_error: 'Area of study is required' }),
-  studyType: z.string({ required_error: 'Study type is required' }),
+  studyType: z.enum(['Remote', 'In-premise'], {
+    required_error: 'Study type must be either "Remote" or "In-premise"',
+  }),
   startDate: z.string({ required_error: 'Start date is required' }),
-  endDate: z.string().optional(),
-  score: z.string().optional(),
-  courses: z.array(z.string({ required_error: 'Course name cannot be empty' })).optional(),
+  endDate: z.string().nullable(),
+  score: z.string().nullable(),
+  courses: z.array(z.string()).nullable(),
 });
 
 const CertificateSchema = z.object({
   name: z.string({ required_error: 'Certificate name is required' }),
   date: z.string({ required_error: 'Date is required' }),
   issuer: z.string({ required_error: 'Issuer is required' }),
-  url: z.string().url('Invalid URL format').optional(),
+  url: z.string().url('Invalid URL format').nullable(),
 });
 
 const SkillSchema = z.object({
   name: z.string({ required_error: 'Skill name is required' }),
-  level: z.string({ required_error: 'Skill level is required' }),
-  keywords: z.array(z.string({ required_error: 'Keyword cannot be empty' })).optional(),
+  level: z.enum(['Novice', 'Proficient', 'Expert'], { required_error: 'Skill level is required' }),
+  keywords: z
+    .array(z.instanceof(ObjectId), { required_error: 'Keyword cannot be empty' })
+    .nullable(),
 });
 
 const LanguageSchema = z.object({
   language: z.string({ required_error: 'Language is required' }),
-  fluency: z.string({ required_error: 'Fluency level is required' }),
+  fluency: z.enum(['Beginner', 'Intermediate', 'Advanced', 'Native'], {
+    required_error: 'Fluency level is required',
+  }),
 });
 
 const InterestSchema = z.object({
   name: z.string({ required_error: 'Interest name is required' }),
-  keywords: z.array(z.string({ required_error: 'Keyword cannot be empty' })).optional(),
+  keywords: z.array(z.string()).nullable(),
 });
 
 const ProjectSchema = z.object({
   name: z.string({ required_error: 'Project name is required' }),
-  startDate: z.string({ required_error: 'Start date is required' }),
-  endDate: z.string({ required_error: 'End date is required' }),
+  startDate: z.date({ required_error: 'Start date is required' }),
+  endDate: z.date().nullable(),
   description: z.string({ required_error: 'Description is required' }),
-  highlights: z.array(z.string({ required_error: 'Highlight cannot be empty' })).optional(),
-  githubUrl: z.string().url('Invalid URL format').optional(),
-  deployedUrl: z.string().url('Invalid URL format').optional(),
+  highlights: z.array(z.string()).nullable(),
+  githubUrl: z.string().url('Invalid URL format').nullable(),
+  deployedUrl: z.string().url('Invalid URL format').nullable(),
+  techStack: z.array(z.instanceof(ObjectId), { required_error: 'Tech stack cannot be empty' }),
 });
 
 const UserSchema = z.object({
   basics: z.object({
     name: z.string({ required_error: 'Name is required' }),
     label: z.string({ required_error: 'Label is required' }),
-    image: z.string().url('Invalid URL format').optional(),
+    image: z.string().url('Invalid URL format').nullable(),
     email: z.string({ required_error: 'Email is required' }).email('Invalid email format'),
     phone: z.string({ required_error: 'Phone number is required' }),
-    url: z.string().url('Invalid URL format').optional(),
+    url: z.string().url('Invalid URL format').nullable(),
     summary: z.string({ required_error: 'Summary is required' }),
     location: LocationSchema,
-    profiles: z.array(ProfileSchema).optional(),
+    profiles: z.array(ProfileSchema).nullable(),
   }),
-  work: z.array(WorkSchema).optional(),
-  education: z.array(EducationSchema).optional(),
-  certificates: z.array(CertificateSchema).optional(),
-  skills: z.array(SkillSchema).optional(),
-  languages: z.array(LanguageSchema).optional(),
-  interests: z.array(InterestSchema).optional(),
-  projects: z.array(ProjectSchema).optional(),
+  work: z.array(WorkSchema).nullable(),
+  education: z.array(EducationSchema).nullable(),
+  certificates: z.array(CertificateSchema).nullable(),
+  skills: z.array(SkillSchema).nullable(),
+  languages: z.array(LanguageSchema).nullable(),
+  interests: z.array(InterestSchema).nullable(),
+  projects: z.array(ProjectSchema).nullable(),
 });
 
 export { UserSchema };
