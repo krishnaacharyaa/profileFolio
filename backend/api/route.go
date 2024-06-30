e930ef31a4f3aa0e3963d07dab0095a4927de98e
Pratham Dupare <108093728+prathamdupare@users.noreply.github.com>
2024-06-30 13:24:28 +0530

    feat: added ai apis (#6)
    Co-authored-by: Krishna R Acharya <116620586+krishnaacharyaa@users.noreply.github.com>

--- backend/api/route.go
+++ backend/api/route.go
@@ -7,6 +7,9 @@ import (
 )
 
 func RegisterUserRoutes(mux *http.ServeMux) {
-    mux.HandleFunc("/api/user", handlers.GetUserHandler)
+	mux.HandleFunc("/api/user", handlers.GetUserHandler)
 	mux.HandleFunc("/api/skills", handlers.GetSkillsHandler)
+	mux.HandleFunc("/api/cover-letter", handlers.ChatHandler)
+	mux.HandleFunc("/api/calc-chance", handlers.CalculateReplacementChance)
+	mux.HandleFunc("/api/resume-review", handlers.ResumeReview)
 }
