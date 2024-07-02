590e25d7b607a3487aaa60b6ce490a3d27afc325
Ritik Jadhav <ritiksjadhav99@gmail.com>
2024-07-02 22:46:21 +0530

    Removed recoil

--- frontend/app/resume-builder/page.tsx
+++ frontend/app/resume-builder/page.tsx
@@ -2,17 +2,13 @@
 import Sidebar from '@/components/common/Sidebar';
 import ResumeHeader from '@/components/resume-builder/ResumeHeader';
 import ResumeView from '@/components/resume-builder/ResumeView';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { ToggleState } from '../store';
-import { AlignJustify, CircleX } from 'lucide-react';
-import { useForm, FormProvider, useFormContext, FieldValues } from 'react-hook-form';
+import { useForm, FormProvider, FieldValues } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserInputSchema } from '../zod/user-inputs';
 import defaultUserInputs from '../store/default-user-inputs';
 
 
 export default function Page() {
-  const [toggle, setToggle] = useRecoilState(ToggleState);
   const methods = useForm({
     resolver: zodResolver(UserInputSchema),
     defaultValues: defaultUserInputs,
@@ -27,22 +23,10 @@ export default function Page() {
 
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)} className="h-full">
-          {toggle ? (
             <div className="mt-7 flex justify-between h-full">
               <Sidebar />
               <ResumeView />
             </div>
-          ) : (
-            <div>
-              <AlignJustify
-                className="cursor-pointer"
-                onClick={() => {
-                  setToggle(!toggle);
-                }}
-              />
-              <ResumeView />
-            </div>
-          )}
           <button type="submit" className='border-2 border-black rounded-md p-4'>Submit</button>
         </form>
       </FormProvider>
