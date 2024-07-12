cc0beb247739ea9664c923ae354f48ce1d67f662
Khumanthem_Lanthoiba_Meitei <96556290+Lanthoiba2022@users.noreply.github.com>
2024-07-12 12:39:40 +0530

    Update radialProfileCard.tsx
    Profile Progress checker function imported from profileCompletionCheck.ts

--- frontend/components/radialProfileCard.tsx
+++ frontend/components/radialProfileCard.tsx
@@ -19,40 +19,8 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import { calculateProfileCompletion, fetchUserData } from '@/utils/profileCompletionCheck';
 
-// get API endpoint BE
-export const fetchUserData = async () => {
-  try {
-    const response = await fetch('http://localhost:8080/api/user'); 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    return null; 
-  }
-};
-
-export const calculateProfileCompletion = (userData: any) => {
-  let completedFields = 0;
-  const totalFields = 10; // Adjust based on your criteria
-
-  // Check if specific fields are filled
-  if (userData.basics?.name) completedFields++; // name
-  if (userData.basics?.label) completedFields++; // label Role
-  if (userData.basics?.image) completedFields++; // image
-  if (userData.basics?.email) completedFields++; // Email
-  if (userData.basics?.phone) completedFields++; // phone
-  if (userData.basics?.url) completedFields++; // url
-  if (userData.projects[0]?.githubUrl) completedFields++; // github
-  if (userData.education[0]?.institution) completedFields++; // education 
-  if (userData.certificates[0]?.name) completedFields++; // certificates
-  if (userData.projects[0].techStack.length > 2) completedFields++; // skills
-
-  return (completedFields / totalFields) * 100;
-};
 
 const RadialProfileCard = () => {
   const [userData, setUserData] = useState(null);
