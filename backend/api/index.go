045034a799bc0b11240eb699a1ab4eaae68ef6d3
krishnaacharyaa <krishnaacharya.official@gmail.com>
2024-07-31 02:09:20 +0530

    chore: test

--- backend/api/index.go
+++ backend/api/index.go
@@ -3,14 +3,23 @@ package api
 import (
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	router := mux.NewRouter()
-	
 	RegisterUserRoutes(router)
-	// Setup other routes here...
-	// fmt.Fprintf(w, "Hello from the main handler!")
-	router.ServeHTTP(w, r)
+
+	// Setup CORS
+	corsMiddleware := handlers.CORS(
+		handlers.AllowedOrigins([]string{"https://profilefolio-theta.vercel.app"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
+	// Wrap the router with the CORS middleware
+	corsHandler := corsMiddleware(router)
+	corsHandler.ServeHTTP(w, r)
 }
