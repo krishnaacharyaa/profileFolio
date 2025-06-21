12ab70d93197ba27b1e4a4c6ae79f743c88eab54
krishnaacharyaa <krishnaacharya.official@gmail.com>
2025-06-21 12:28:05 +0530

    fix

--- services/resume/pkg/worker.go
+++ services/resume/pkg/worker.go
@@ -14,11 +14,11 @@ type AccountCreatedEventData struct {
 }
 
 func AccountCreated(ctx context.Context, input inngestgo.Input[AccountCreatedEventData]) (any, error) {
-	fmt.Printf("Received event: %v\n", input.Event.Data.UserID)
+	fmt.Printf("Received event: %v\n", input.Event.UserID)
 
 	// Define a step (e.g., send a follow-up email)
 	_, err := step.Run(ctx, "send-follow-up-email", func(ctx context.Context) (any, error) {
-		fmt.Printf("Sending follow-up email for account: %s\n", input.Event.Data.UserID)
+		fmt.Printf("Sending follow-up email for account: %s\n", input.Event.UserID)
 		time.Sleep(1 * time.Second) // Simulate work
 		return true, nil
 	})
