bb1829bcede8df196585358d27034aec34664c9a
krishnaacharyaa <krishnaacharya.official@gmail.com>
2025-06-21 13:04:35 +0530

    fix

--- services/resume/api/index.go
+++ services/resume/api/index.go
@@ -91,6 +91,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Failed to create function: %v\n", err)
 		return
 	}
+	router.Any("/api/inngest", gin.WrapH(inngestClient.Serve()))
 	// Serve the request
 	router.ServeHTTP(w, r)
 }
